dcf27757ac317b6bf5a938fa8dd21676d0577d5f
Connor <connor.n.bell@gmail.com>
2012-12-08 19:49:49 -0400

    Basic rematch system

--- Assets/ControllerToUseController.cs
+++ Assets/ControllerToUseController.cs
@@ -19,6 +19,47 @@ public class ControllerToUseController : MonoBehaviour {
 	public float health = 124;
 	public float energy = 124;
 	public bool movementEnabled = false;
+	public bool rematchAccepted = false;
+	public bool playerAcceptedRematch = false;
+	
+	void OnGUI () {
+		
+		if (health <= 0) { // The player has died.
+			if (networkView.isMine) {
+				GUI.Label(new Rect(Screen.width/2, Screen.height/2-20, 200, 200), "You lose");
+			} else {
+				GUI.Label(new Rect(Screen.width/2, Screen.height/2-20, 200, 200), "You Win!");
+			}
+
+			if (!playerAcceptedRematch && rematchAccepted) {
+			
+				GUI.Label(new Rect(Screen.width/2, Screen.height/2-50, 200, 200), "Waiting for player...");				
+			
+			}
+			
+			if (playerAcceptedRematch && rematchAccepted) {
+						
+				if (networkView.isMine) {
+					movementEnabled = true;
+				}
+				
+				transform.position = spawnPosition;
+				health = 124;
+				energy = 124;	
+				return;
+			}
+				
+			if (GUI.Button(new Rect(Screen.width/2-110, Screen.height/2, 220, 100), "Rematch!")) {
+				
+				rematchAccepted = true;
+				NetworkViewID viewID = Network.AllocateViewID();
+				networkView.RPC("recieveRematchRequest", RPCMode.OthersBuffered, viewID);
+			}
+		} else {
+			rematchAccepted = false;
+			playerAcceptedRematch = false;
+		}	
+	}
 	
 	void Start() {
 		spawnPosition = transform.position;
@@ -73,14 +114,11 @@ public class ControllerToUseController : MonoBehaviour {
 			isMov=1;
 		}
 		
-		if(rBod.velocity.magnitude > maxVel){rBod.velocity =rBod.velocity.normalized*maxVel;}
+		if (rBod.velocity.magnitude > maxVel) {
+			rBod.velocity = rBod.velocity.normalized*maxVel;
+		}
 		
-		if(health <= 0){
-			transform.position = spawnPosition;
-			health = 124;
-			energy = 124;
-			}
-		if(energy <= 124 && !Input.GetMouseButton(0)){
+		if (energy <= 124 && !Input.GetMouseButton(0)){
 			energy += 0.25f;
 		}
 		
@@ -95,5 +133,10 @@ public class ControllerToUseController : MonoBehaviour {
 	}
 	void Damage(){
 		health -= 0.5f;
+	}
+	
+	[RPC]
+	void recieveRematchRequest(NetworkViewID viewID) {
+		playerAcceptedRematch = true;	
 	}
 }
